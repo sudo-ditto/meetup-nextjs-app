8bf5591933b49a543984479aa3cbc3ce8ac404bf
Loretta <loreta.krasteva@brandmaker.com>
2021-11-17 15:33:48 +0200

    MeetupDetail component styles added

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import classes from './MeetupDetail.module.css';
 
-const MeetupDetail = ({ image, title, address, description }) => {
+function MeetupDetail(props) {
     return (
-        <>
+        <section className={classes.detail}>
             <img
-                src={image}
-                alt={title}
+                src={props.image}
+                alt={props.title}
             />
-            <h1>{title}</h1>
-            <address>{address}</address>
-            <p>{description}</p>
-        </>
-    )
+            <h1>{props.title}</h1>
+            <address>{props.address}</address>
+            <p>{props.description}</p>
+        </section>
+    );
 }
 
-export default MeetupDetail
+export default MeetupDetail;
